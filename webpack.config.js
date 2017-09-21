d282e30519dafe3307b45b996d63b5b3811f596f
pavel antsiferov <pollop88@yandex.ru>
2017-09-21 09:49:43 +0300

    add dev config, add prod config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         'index':  PATHS.source + '/pages/index/index.js',
         'blog':  PATHS.source + '/pages/blog/blog.js',
@@ -41,8 +41,27 @@ module.exports = {
                 ]
             }
         ]
-    },
-    devServer: {
-        stats: 'errors-only'
     }
+}
+
+const developmentConfig = {
+    devServer: {
+        stats: 'errors-only',
+        port: 9000
+    }
+};
+
+module.exports = function (env) {
+    if(env === 'production'){
+        return common
+    }
+
+    if(env === 'development'){
+        return Object.assign(
+            {},
+            common,
+            developmentConfig
+        )
+    }
+    
 }
