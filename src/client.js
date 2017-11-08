3333310baf21401cbb06e87d565e845505a27b0f
pavel antsiferov <pollop88@yandex.ru>
2017-11-08 11:37:49 +0300

    add router

--- src/client.js
+++ src/client.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Route} from 'react-router'
-
-import Temp from './components/Temp/Temp'
-import Main from './components/Main/Main'
-
-const Router = () => (
-    <Route>
-        <Route path="/" component={Main}/>
-        <Route path="/temp" component={Temp}/>
-    </Route>
-)
+import Routes from './routes'
 
 
-ReactDOM.render(<Router/>, document.getElementById('container'))
+ReactDOM.render(<Routes/>, document.getElementById('container'))
 
 
 
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router, Route, browserHistory } from 'react-router'
+
+import Temp from './components/Temp/Temp'
+import Main from './components/Main/Main'
+
+
+
+export default () => (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}/>
+        <Route path="/temp" component={Temp}/>
+    </Router>
+)
+
